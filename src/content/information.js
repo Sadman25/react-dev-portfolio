128dc1f9b170573e6bf9624387d696baca1f80ce
Sadman25 <shadman.saqif@gmail.com>
2023-12-21 04:28:19 +0600

    picture uploaded

--- src/content/information.js
+++ src/content/information.js
@@ -4,7 +4,7 @@ const userData = {
   firstName: "<Shadman",
   lastName: "Saqif/>",
   title: "About Me",
-  img: "https://pics.craiyon.com/2023-10-30/66ff0c95358e458c859ecea5d3764b32.webp",
+  img: "/me.jpg",
   description:
     "Hey there! 👋 I'm Shadman, a passionate Computer Science & Engineering graduate from North South University, Bangladesh. 💻 By day, I'm a sleeping giant, and by night, I transform into an owl, a tech enthusiast exploring the wonders of coding. 🌌 My journey spans from crafting code to delving into the realms of Python, Javascript. When not in the digital world, you'll find me navigating the strategic landscapes of Various games on my PS5 (I guess it's another digital world). Let's connect and embark on an exciting adventure through the vast universe of technology! 🚀 ",
 };
